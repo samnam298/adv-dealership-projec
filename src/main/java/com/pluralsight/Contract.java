f4b8108d9ece93b7579402ca54af2e5a5d590d83
samnam298 <samrawithailena@gmail.com>
2025-11-04 09:49:34 -0500

    Added instance variables and constructor for Contract

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -2,4 +2,15 @@ package com.pluralsight;
 
 public abstract class Contract {
 
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
 }
