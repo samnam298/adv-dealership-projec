9392f7697559bbef3721775058600ca7dcfb808c
samnam298 <samrawithailena@gmail.com>
2025-11-04 11:50:29 -0500

    Implemented saveContract to append contract data based on type

--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -1,9 +1,60 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ContractFileManager {
 
     private static final String FILE_NAME = "contracts.csv";
 
     public void saveContract(Contract contract) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+
+            Vehicle v = contract.getVehicleSold();
+
+            if (contract instanceof SalesContract sc) {
+                writer.write("SALE|" +
+                        sc.getDate() + "|" +
+                        sc.getCustomerName() + "|" +
+                        sc.getCustomerEmail() + "|" +
+                        v.getVin() + "|" +
+                        v.getYear() + "|" +
+                        v.getMake() + "|" +
+                        v.getModel() + "|" +
+                        v.getType() + "|" +
+                        v.getColor() + "|" +
+                        v.getOdometer() + "|" +
+                        v.getPrice() + "|" +
+                        sc.getSalesTaxAmount() + "|" +
+                        sc.getRecordingFee() + "|" +
+                        sc.getProcessingFee() + "|" +
+                        sc.getTotalPrice() + "|" +
+                        (sc.isFinanced() ? "YES" : "NO") + "|" +
+                        sc.getMonthlyPayment());
+            }
+            else if (contract instanceof LeaseContract lc) {
+                writer.write("LEASE|" +
+                        lc.getDate() + "|" +
+                        lc.getCustomerName() + "|" +
+                        lc.getCustomerEmail() + "|" +
+                        v.getVin() + "|" +
+                        v.getYear() + "|" +
+                        v.getMake() + "|" +
+                        v.getModel() + "|" +
+                        v.getType() + "|" +
+                        v.getColor() + "|" +
+                        v.getOdometer() + "|" +
+                        v.getPrice() + "|" +
+                        lc.getExpectedEndingValue() + "|" +
+                        lc.getLeaseFee() + "|" +
+                        lc.getTotalPrice() + "|" +
+                        lc.getMonthlyPayment());
+            }
+
+            writer.newLine();
+
+        } catch (IOException e) {
+        }
     }
 }
