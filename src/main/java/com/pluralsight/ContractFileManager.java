3ccc1da69ede4d48d1081a6e4bb52b2cb75809f6
samnam298 <samrawithailena@gmail.com>
2025-11-04 11:49:08 -0500

    Prepared the filename constant and the method that will handle saving

---
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ContractFileManager {
+
+    private static final String FILE_NAME = "contracts.csv";
+
+    public void saveContract(Contract contract) {
+    }
+}
