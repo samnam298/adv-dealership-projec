e74607002568afe6efb4bbec0910c2115537c663
samnam298 <samrawithailena@gmail.com>
2025-11-04 09:52:20 -0500

    Declared abstract methods for total price and monthly payment

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -15,14 +15,10 @@ public abstract class Contract {
     }
 
     public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-
     public String getCustomerName() { return customerName; }
-    public void setCustomerName(String customerName) { this.customerName = customerName; }
-
     public String getCustomerEmail() { return customerEmail; }
-    public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
-
     public Vehicle getVehicleSold() { return vehicleSold; }
-    public void setVehicleSold(Vehicle vehicleSold) { this.vehicleSold = vehicleSold; }
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
